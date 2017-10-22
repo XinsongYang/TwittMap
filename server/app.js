fd790866755318fd51afdb6cd8da1719c36206e1
Xinsong Yang <yangxinsong93@gmail.com>
2017-10-22 04:42:26 -0400

    deploy

--- server/app.js
+++ server/app.js
@@ -45,5 +45,6 @@ app.use(static(
     path.join(__dirname, './public')
 ))
 
-app.listen(3000);
+const port = process.env.PORT || 3000,
+app.listen(port);
 console.log('app started at port 3000...');
